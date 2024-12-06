da33b650728d88046647a187e21bad03535f75e9
cusgan <fmfgbascug@gmail.com>
2024-12-07 00:05:17 +0800

    updated

--- Backpropagation Activity/Form1.Designer.cs
+++ Backpropagation Activity/Form1.Designer.cs
@@ -28,12 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            btnOrTrain = new Button();
+            btnOrTest = new Button();
+            btnOrCreate = new Button();
+            tbOrResult = new TextBox();
+            tbOrB = new TextBox();
+            tbOrA = new TextBox();
+            tabPage2 = new TabPage();
+            btnAndTrain = new Button();
+            btnAndTest = new Button();
+            btnAndCreate = new Button();
+            tbAndResult = new TextBox();
+            tbAndD = new TextBox();
+            tbAndC = new TextBox();
+            tbAndB = new TextBox();
+            tbAndA = new TextBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(652, 383);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(btnOrTrain);
+            tabPage1.Controls.Add(btnOrTest);
+            tabPage1.Controls.Add(btnOrCreate);
+            tabPage1.Controls.Add(tbOrResult);
+            tabPage1.Controls.Add(tbOrB);
+            tabPage1.Controls.Add(tbOrA);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(644, 350);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "1. OR Gate";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnOrTrain
+            // 
+            btnOrTrain.Location = new Point(248, 229);
+            btnOrTrain.Name = "btnOrTrain";
+            btnOrTrain.Size = new Size(161, 72);
+            btnOrTrain.TabIndex = 5;
+            btnOrTrain.Text = "Train \r\nNeural Network";
+            btnOrTrain.UseVisualStyleBackColor = true;
+            btnOrTrain.Click += btnOrTrain_Click;
+            // 
+            // btnOrTest
+            // 
+            btnOrTest.Location = new Point(431, 229);
+            btnOrTest.Name = "btnOrTest";
+            btnOrTest.Size = new Size(161, 72);
+            btnOrTest.TabIndex = 4;
+            btnOrTest.Text = "Test\r\nNeural Network";
+            btnOrTest.UseVisualStyleBackColor = true;
+            btnOrTest.Click += btnOrTest_Click;
+            // 
+            // btnOrCreate
+            // 
+            btnOrCreate.Location = new Point(57, 229);
+            btnOrCreate.Name = "btnOrCreate";
+            btnOrCreate.Size = new Size(161, 72);
+            btnOrCreate.TabIndex = 3;
+            btnOrCreate.Text = "Create\r\nNew BPNN";
+            btnOrCreate.UseVisualStyleBackColor = true;
+            btnOrCreate.Click += btnOrCreate_Click;
+            // 
+            // tbOrResult
+            // 
+            tbOrResult.Location = new Point(356, 118);
+            tbOrResult.Name = "tbOrResult";
+            tbOrResult.Size = new Size(236, 27);
+            tbOrResult.TabIndex = 2;
+            // 
+            // tbOrB
+            // 
+            tbOrB.Location = new Point(57, 150);
+            tbOrB.Name = "tbOrB";
+            tbOrB.Size = new Size(236, 27);
+            tbOrB.TabIndex = 1;
+            // 
+            // tbOrA
+            // 
+            tbOrA.Location = new Point(57, 84);
+            tbOrA.Name = "tbOrA";
+            tbOrA.Size = new Size(236, 27);
+            tbOrA.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(btnAndTrain);
+            tabPage2.Controls.Add(btnAndTest);
+            tabPage2.Controls.Add(btnAndCreate);
+            tabPage2.Controls.Add(tbAndResult);
+            tabPage2.Controls.Add(tbAndD);
+            tabPage2.Controls.Add(tbAndC);
+            tabPage2.Controls.Add(tbAndB);
+            tabPage2.Controls.Add(tbAndA);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(644, 350);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "2. AND Gate";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnAndTrain
+            // 
+            btnAndTrain.Location = new Point(248, 229);
+            btnAndTrain.Name = "btnAndTrain";
+            btnAndTrain.Size = new Size(161, 72);
+            btnAndTrain.TabIndex = 8;
+            btnAndTrain.Text = "Train \r\nNeural Network";
+            btnAndTrain.UseVisualStyleBackColor = true;
+            btnAndTrain.Click += btnAndTrain_Click;
+            // 
+            // btnAndTest
+            // 
+            btnAndTest.Location = new Point(431, 229);
+            btnAndTest.Name = "btnAndTest";
+            btnAndTest.Size = new Size(161, 72);
+            btnAndTest.TabIndex = 7;
+            btnAndTest.Text = "Test\r\nNeural Network";
+            btnAndTest.UseVisualStyleBackColor = true;
+            btnAndTest.Click += btnAndTest_Click;
+            // 
+            // btnAndCreate
+            // 
+            btnAndCreate.Location = new Point(57, 229);
+            btnAndCreate.Name = "btnAndCreate";
+            btnAndCreate.Size = new Size(161, 72);
+            btnAndCreate.TabIndex = 6;
+            btnAndCreate.Text = "Create\r\nNew BPNN";
+            btnAndCreate.UseVisualStyleBackColor = true;
+            btnAndCreate.Click += btnAndCreate_Click;
+            // 
+            // tbAndResult
+            // 
+            tbAndResult.Location = new Point(356, 118);
+            tbAndResult.Name = "tbAndResult";
+            tbAndResult.Size = new Size(236, 27);
+            tbAndResult.TabIndex = 5;
+            // 
+            // tbAndD
+            // 
+            tbAndD.Location = new Point(57, 183);
+            tbAndD.Name = "tbAndD";
+            tbAndD.Size = new Size(236, 27);
+            tbAndD.TabIndex = 4;
+            // 
+            // tbAndC
+            // 
+            tbAndC.Location = new Point(57, 150);
+            tbAndC.Name = "tbAndC";
+            tbAndC.Size = new Size(236, 27);
+            tbAndC.TabIndex = 3;
+            // 
+            // tbAndB
+            // 
+            tbAndB.Location = new Point(57, 117);
+            tbAndB.Name = "tbAndB";
+            tbAndB.Size = new Size(236, 27);
+            tbAndB.TabIndex = 2;
+            // 
+            // tbAndA
+            // 
+            tbAndA.Location = new Point(57, 84);
+            tbAndA.Name = "tbAndA";
+            tbAndA.Size = new Size(236, 27);
+            tbAndA.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(677, 407);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Back Propagation";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TextBox tbOrB;
+        private TextBox tbOrA;
+        private TextBox tbOrResult;
+        private Button btnOrTrain;
+        private Button btnOrTest;
+        private Button btnOrCreate;
+        private TextBox tbAndResult;
+        private TextBox tbAndD;
+        private TextBox tbAndC;
+        private TextBox tbAndB;
+        private TextBox tbAndA;
+        private Button btnAndTrain;
+        private Button btnAndTest;
+        private Button btnAndCreate;
     }
 }
--- Backpropagation Activity/Form1.cs
+++ Backpropagation Activity/Form1.cs
@@ -1,3 +1,5 @@
+using Backprop;
+
 namespace Backpropagation_Activity
 {
     public partial class Form1 : Form
@@ -5,6 +7,94 @@ namespace Backpropagation_Activity
         public Form1()
         {
             InitializeComponent();
+        }
+
+        NeuralNet orNN = new(2, 2, 1);
+
+        private void btnOrCreate_Click(object sender, EventArgs e)
+        {
+            orNN = new NeuralNet(2, 2, 1);
+        }
+        private void btnOrTest_Click(object sender, EventArgs e)
+        {
+            orNN.setInputs(0, Convert.ToDouble(tbOrA.Text));
+            orNN.setInputs(1, Convert.ToDouble(tbOrB.Text));
+            orNN.run();
+            tbOrResult.Text = "" + orNN.getOuputData(0).ToString("F7");
+        }
+        private void btnOrTrain_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < 1000; x++)
+            {
+                orNN.setInputs(0, 0.0);
+                orNN.setInputs(1, 0.0);
+                orNN.setDesiredOutput(0, 0.0);
+                orNN.learn();
+
+                orNN.setInputs(0, 1.0);
+                orNN.setInputs(1, 0.0);
+                orNN.setDesiredOutput(0, 1.0);
+                orNN.learn();
+
+                orNN.setInputs(0, 0.0);
+                orNN.setInputs(1, 1.0);
+                orNN.setDesiredOutput(0, 1.0);
+                orNN.learn();
+
+                orNN.setInputs(0, 1.0);
+                orNN.setInputs(1, 1.0);
+                orNN.setDesiredOutput(0, 1.0);
+                orNN.learn();
+            }
+        }
+
+
+        const int HIDDEN = 4;
+        const int EPOCH = 10000;
+        NeuralNet andNN = new(4, HIDDEN, 1);
+        private void btnAndCreate_Click(object sender, EventArgs e)
+        {
+            andNN = new(4, HIDDEN, 1);
+        }
+
+        private void btnAndTest_Click(object sender, EventArgs e)
+        {
+            andNN.setInputs(0, Convert.ToDouble(tbAndA.Text));
+            andNN.setInputs(1, Convert.ToDouble(tbAndB.Text));
+            andNN.setInputs(2, Convert.ToDouble(tbAndC.Text));
+            andNN.setInputs(3, Convert.ToDouble(tbAndD.Text));
+            andNN.run();
+            tbAndResult.Text = "" + andNN.getOuputData(0).ToString("F7");
+        }
+
+        private void btnAndTrain_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < EPOCH; x++)
+            {
+                andNN.setDesiredOutput(0, 0.0);
+                for (int y=0; y < 15; y++)
+                {
+                    andNN.setInputs(0, getBitDouble(y, 0));
+                    andNN.setInputs(1, getBitDouble(y, 1));
+                    andNN.setInputs(2, getBitDouble(y, 2));
+                    andNN.setInputs(3, getBitDouble(y, 3));
+                    andNN.learn();
+                }
+                andNN.setInputs(0, 1.0);
+                andNN.setInputs(1, 1.0);
+                andNN.setInputs(2, 1.0);
+                andNN.setInputs(3, 1.0);
+                andNN.setDesiredOutput(0, 1.0);
+                andNN.learn();
+            }
+        }
+        private double getBitDouble(int num, int pos)
+        {
+            int res = num & (1 << pos);
+            if (res == 0)
+                return 0.0;
+            else
+                return 1.0;
         }
     }
 }
